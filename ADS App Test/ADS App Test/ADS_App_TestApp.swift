8118e7c041b135a24a66502018f9e16a343a46df
Aaron Beckley <45105699+ambeckley@users.noreply.github.com>
2024-03-21 12:33:17 -0400

    Add files via upload

---
+++ ADS App Test/ADS App Test/ADS_App_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADS_App_TestApp.swift
+//  ADS App Test
+//
+//  Created by Aaron Beckley on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ADS_App_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
